cf6457c6b6fed779a297f8b787e73f28ffb36b8b
CreateModMaster <alex.inns@caterhamschool.co.uk>
2024-10-02 12:09:19 +0000

    deleted useless python file

--- main.py
+++
@@ -1,9 +0,0 @@
-strtoturn = input("Enter a string: ")
-encstr = strtoturn.encode()
-hexstr = encstr.hex()
-print(hexstr)
-hexint = int(hexstr, 16)
-hexbin = bin(hexint)
-binary_string = hexbin[2:]
-print(binary_string)
-strtodecode = str
